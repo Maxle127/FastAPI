07556b600f84442e7413a5ababff154e9a22cae3
Maxle127 <maksimleiza@gmail.com>
2026-02-10 20:21:43 +0200

    Update

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -25,7 +25,7 @@ ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-oauth2_bearer= OAuth2PasswordBearer(tokenUrl="auth/token")
+oauth2_bearer= OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 class CreateUserRequest(BaseModel):
     username: str
